2bb4bfaa9c11a59afca3ddc6959a90e2bf24db77
raulMrello <raul.martin.rello@gmail.com>
2015-03-25 09:48:49 +0100

    Serial -> TCP bridge working!

--- include/user_config.h
+++ include/user_config.h
@@ -2,9 +2,9 @@
 #define __USER_CONFIG_H__
 
 #define USE_WIFI_MODE		STATIONAP_MODE
-#define WIFI_CLIENTSSID		"QC_rc"
+#define WIFI_CLIENTSSID		"QC_wifi"
 #define WIFI_CLIENTPASSWORD	"r231190m"
-#define WIFI_AP_NAME		"QC_wifi"
+#define WIFI_AP_NAME		"RC_client"
 #define WIFI_AP_PASSWORD	"r231190m"
 #define TCPSERVERIP			"192.168.5.1"
 #define TCPSERVERPORT		80
