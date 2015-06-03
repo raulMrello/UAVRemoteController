84a9b7970e0012d9c0f4955ddc81f68166932563
raulMrello <raul.martin.rello@gmail.com>
2015-06-03 04:14:50 +0200

    Update 03.06.2015-001

--- Project_mbed_stm32f10x_src/user-modules/Hsm.h
+++ Project_mbed_stm32f10x_src/user-modules/Hsm.h
@@ -1,226 +1,102 @@
-/** ###################################################################
-**     Filename  : StateFramework.h
-**     Date/Time : 18.02.2009
-**     Abstract  :
-**         Módulo de gestión del Framework de estados, basado en la
-**          metodología de Miro Samek (Quantum Leaps), pero mucho más 
-**          ligero y optimizado.
-**          No es 100% UML-compliant, pero implementa correctamente los
-**          eventos ENTRY, EXIT, así como la herencia en las máquinas de
-**          estados jerárquicas.
-**         
-**         NOTAS: En los eventos EXIT, no se pueden realizar transiciones
-**                a otras máquinas, ya que estos eventos sólo se utilizan
-**                como destructores de datos. Sin embargo, en los eventos
-**                ENTRY si es posible saltar a submáquinas.
-**     Settings  :
-**     Contents  :
-**     Realizado :
-**         Raul Martin
-** ###################################################################*/
-#ifndef _STATEFRAMEWORK_H
-#define _STATEFRAMEWORK_H
+/*
+ * Hsm.h
+ *
+ *  Created on: 06/06/2015
+ *      Author: raulMrello
+ *
+ *  Hsm provides state machine semantics to any application.
+ */
 
-/****************************************************************************************************//**
- *                        REQUIRED LIBRARIES
- ********************************************************************************************************/
+#ifndef HSM_H_
+#define HSM_H_
+
+
+#define HSM_RELEASE   "0.x.20150603001"
+
 #include <stdint.h>
-#include <RTL.h>		// porting to RTX RTOS critical section 
-#define EnterCritical()			tsk_lock()
-#define ExitCritical()			tsk_unlock()
+/** Add here your porting code for RTOS-based applications */
+#include "rtos.h"
 
 
-namespace HSM{
+namespace HSM {
+
+#define EnterCritical()			//tsk_lock()
+#define ExitCritical()			//tsk_unlock()
+
+/** Typedefs */	
+typedef uint8_t State;
+typedef void    Object;
+
+/** \enum Signals
+ *  \brief Signal enumeration. Users can extend them from USER_SIG value. These are reserved.
+ */
+enum Signals {
+	INIT_SIG = 0,
+	ENTRY_SIG,		        /**< evento de entrada */
+	EXIT_SIG,               /**< evento de salida */
+	TIMEOUT_SIG,			/**< evento de timeout */
+	USER_SIG                /**< primer evento libre para el usuario */
+};
+
 	
-	typedef uint8_t State;
-	typedef void    Object;
-	typedef State (*StateHandler)(Object *pThis, Event const *e);
-	
-	#define RET_HANDLED       ((State)0u)     /**< indica que el evento se ha procesado */
-	#define RET_IGNORED       ((State)1u)     /**< indica que el evento se ha controlado pero se ignora */
-	#define RET_TRAN          ((State)2u)     /**< indica que hay una transición de estados */
-	#define RET_SUPER         ((State)3u)     /**< indica que el evento se ha delegado al estado padre */	
-	
-	#define HANDLED()		return(RET_HANDLED)    
-	#define IGNORED()       return(RET_IGNORED)
-	#define TRAN(st_)       {((StateMachine *)me)->target = (StateHandler)st_;return(RET_TRAN);}
-	#define SUPER(st_)      {((StateMachine *)me)->target = (StateHandler)(st_);return(RET_SUPER);}
-	#define DELEGATE(st_)	SUPER(st_)
-	
-	class Event{
-	 public:
-		 enum ReservedSignals {
-			ENTRY_SIG = 1,          /**< evento de entrada */
-			EXIT_SIG,               /**< evento de salida */
-			TIMEOUT_SIG,			/**< evento de timeout */
-			USER_SIG                /**< primer evento libre para el usuario */
-		};
-		uint32_t sig;               /**< código del evento */
+/** \class Event
+ *  \brief This class provides Event definition capabilities for inheritance
+ */
+class Event{
+ public:	 
+	 /** Constructor */
+	 Event(uint32_t sig){ _sig = sig; }
+	 /** Getter/Setter */
+	 uint32_t getSig(){ return _sig; }
+	 void setSig(uint32_t sig){ _sig = sig; }
+ private:
+	 uint32_t _sig;      /**< event signal */
+};
+
+/** \class Hsm
+ *  \brief This class provides Hierarchical State Machine semantics
+ */
+class Hsm {
+public:
+	/** Interface for inheritance */
+virtual State init(Event const * e) = 0;     /**< manejador del estado Initial */
+	virtual State top(Event const * e) = 0;      /**< manejador del estado Padre */
+	/** Constructor */
+	Hsm();
+	/** Destructor */
+	virtual ~Hsm();
+	/** Public methods */
+	void dispatch(Event const *e);
+private:
+	State (Hsm::*_init)(Event const *e);
+	State (Hsm::*_state)(Event const *e);
+	State (Hsm::*_target)(Event const *e);
+	State retFromState(uint8_t retVal, State (Hsm::*state)(Event const *));	
+	Event *_timeoutEvt;
+	enum DispatchingFlags{
+		FIRST_TIME  = 1u,
+		EXIT_NOW    = 2u,
 	};
-	
-	class Hsm{
-	 public:
-		
-	 protected:
-		StateHandler init;     /**< manejador del estado Initial */
-		StateHandler state;    /**< manejador del estado actual */
-		StateHandler target;   /**< manjeador del estado al que conmutar */
-	 private:
-		
-	 
-	};
-}
 
+};
 
+/** Typedef for Event handling */
+typedef State (Hsm::*StateHandler)(Event const *e);
 
-extern const Event timeoutEvt;
-
-/**************************************************************
-            Tipo definido para cada manejador de estado
-            Nombre: StateHandler
-            Param0: me (componente activo)
-            Param1: e  (evento)
-**************************************************************/
-
-typedef State (*StateHandler)(Object *pThis, Event const *e);
-
-/**************************************************************
-            Estructura de una máquina de estados jerárquica            
-**************************************************************/
-
-typedef struct{
-    StateHandler init;     /**< manejador del estado Initial */
-    StateHandler state;    /**< manejador del estado actual */
-    StateHandler target;   /**< manjeador del estado al que conmutar */
-} StateMachine;
-
-/**************************************************************
-            Tipo de datos devueltos por los manejadores de estado            
-**************************************************************/
-
+/** Common transition definitions and event consuming flags */
 #define RET_HANDLED       ((State)0u)     /**< indica que el evento se ha procesado */
 #define RET_IGNORED       ((State)1u)     /**< indica que el evento se ha controlado pero se ignora */
 #define RET_TRAN          ((State)2u)     /**< indica que hay una transición de estados */
-#define RET_SUPER         ((State)3u)     /**< indica que el evento se ha delegado al estado padre */
-
-
-
-/*
-** ==================================================================================================
-**     Method      :  StateMachine_Ctor
-**
-**     Description :
-**         Constructor de una máquina de estados. Asigna al miembro <init> la función pasada como
-**          argumento, que se corresponde con el estado Initial.
-**     Parameters  :
-**          me:     Componente que gestiona la máquina de estados
-**          Init:   Función que maneja el estado Initial
-**     Return :
-** ==================================================================================================
-*/
-void StateMachine_Ctor(Object *pThis, StateHandler Init);
-
-
-
-/*
-** ==================================================================================================
-**     Method      :  StateMachine_Init
-**
-**     Description :
-**         Invoca la ejecución del dispatcher sin ningún evento. Esto implica que se ejecute el 
-**          primer manejador, que se corresponde con el del estado Initial.
-**     Parameters  :
-**          me:     Componente que gestiona la máquina de estados
-**     Return :
-** ==================================================================================================
-*/
-void StateMachine_Init(Object *pThis);
-
-
-
-/*
-** ==================================================================================================
-**     Method      :  StateMachine_Top
-**
-**     Description :
-**         Es el manejador del superestado de mayor rango en la jerarquía. Lo único que hace es manejar
-**          los eventos delegados por los estados hijo, ignorándolos.
-**     Parameters  :
-**          me:     Componente que gestiona la máquina de estados
-**          e:      evento delegado desde los estados hijo.
-**     Return :
-** ==================================================================================================
-*/
-State StateMachine_Top(Object *pThis, Event const *e);
-
-
-
-/*
-** ==================================================================================================
-**     Method      :  StateMachine_Dispatch
-**
-**     Description :
-**         Es el dispatcher del Framework. Se encarga de evaluar a qué estado hay que ceder el control
-**          y cuál es el evento que se procesa en cada momento.
-**     Parameters  :
-**          me:     Componente que gestiona la máquina de estados
-**          e:      evento que invoca al dispatcher.
-**     Return :
-** ==================================================================================================
-*/
-void StateMachine_Init(Object *pThis);
-State StateMachine_Top(Object *pThis, Event const *e);
-void StateMachine_Dispatch(Object *pThis, Event const *e);
-
-
-
-/*
-** ==================================================================================================
-**     Method      :  ReturningFromState
-**
-**     Description :
-**         Este método permite que la llamada a las macros TRAN, HANDLED, IGNORED, SUPER, THROW tenga
-**          una terminación coherente con la forma de trabajo del dispatcher. Se encarga de fijar el
-**          estado a cambiar (target) en una operación TRAN y de devolver en cada caso el identificador
-**          del tipo de macro ejecutada.
-**     Parameters  :
-**          vme:    Componente que gestiona la máquina de estados
-**          retVal: Código correspondiente al tipo de macro ejecutada
-**          state:  Estado objetivo.
-**     Return :
-** ==================================================================================================
-*/
-State ReturningFromState(Object *pThis, State retVal, StateHandler state);
-
-
-
-/**************************************************************
-            Macros de finalización de estado
-            
-            HANDLED: permite terminar un estado indicando que
-                     el evento se ha procesado.
-            IGNORED: permite terminar un estado indicando que
-                     el evento se ha procesado ignorándolo.
-            TRAN:    permite terminar un estado indicando al
-                     dispatcher el nuevo estado al que conmutar
-                     (target_).
-            SUPER:   permite terminar un estado indicando al
-                     dispatcher que el evento no se ha controlado
-                     y que debe delegarse al estado padre (super_).                            
-**************************************************************/
-
+#define RET_SUPER         ((State)3u)     /**< indica que el evento se ha delegado al estado padre */	
 #define HANDLED()		return(RET_HANDLED)    
 #define IGNORED()       return(RET_IGNORED)
-    
-#define TRAN(st_)       {((StateMachine *)me)->target = (StateHandler)st_;return(RET_TRAN);}
-
-#define SUPER(st_)      {((StateMachine *)me)->target = (StateHandler)(st_);return(RET_SUPER);}
-
+#define TRAN(st_)       {_target = (st_);return(RET_TRAN);}
+#define SUPER(st_)      {_target = (Hsm::*StateHandler)(st_);return(RET_SUPER);}
 #define DELEGATE(st_)	SUPER(st_)
 
-//#define TRAN(st_)       ((StateMachine*)me)->target=(StateHandler)st_;return(RET_TRAN)
-//#define TRAN(target_)  (ReturningFromState(me, RET_TRAN, target_))
-//#define SUPER(super_)  (ReturningFromState(me, RET_SUPER, super_))
+
+} /* namespace */
+
 
 
 #endif
